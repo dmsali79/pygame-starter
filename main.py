b7674140e4d59f8385710b3b1465baf19c335d59
Diaco Salih <dmsali79>
2020-02-11 10:46:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ pygame.init()
 
 # Create the window
 win = pygame.display.set_mode((800, 600))
-img = pygame.image.load('assets/forest-assets/stairs.png').convert()
+
+# Create the font
+font = pygame.font.SysFont("arial", 72)
+bfont = pygame.font.SysFont("arial", 52)
+cfont = pygame.font.SysFont("arial", 42)
+
+text = font.render("The Room.", True, (255, 100, 10))
+btext = bfont.render("The Room.", True, (220, 80, 5))
+ctext = cfont.render("The Room.", True, (180, 60, 1))
+
+img = pygame.image.load('C:/Users/dmsali79/Downloads/dungeon_tiles.png').convert()
+
+img = pygame.image.load('assets/forest-assets/floor.png')
+
 run = True
 while run:
   for event in pygame.event.get():
@@ -13,6 +26,10 @@ while run:
 
 # Game code starts here ---------------------
   win.fill((0, 0, 0))
+  
+  win.blit(text, (125, 1))
+  win.blit(btext, (100, 20))
+  win.blit(ctext, (75, 40))
 
 
   win.blit(img, (400, 300))
