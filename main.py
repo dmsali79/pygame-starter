7db64f253736d0d99821537ab830235fe565abcc
Diaco Salih <dmsali79>
2020-02-11 11:17:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ ctext = cfont.render("The Room.", True, (180, 60, 1))
 img = pygame.image.load('C:/Users/dmsali79/Downloads/dungeon_tiles.png').convert()
 bimg = pygame.image.load('assets/forest-assets/floor.png')
 cimg = pygame.image.load('assets/forest-assets/wall-e-1.png')
+
+c1img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c2img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c3img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c4img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c5img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c6img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c7img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c8img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c9img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+c10img = pygame.image.load('assets/forest-assets/wall-e-1.png')
+
 dimg = pygame.image.load('assets/forest-assets/door.png')
 eimg = pygame.image.load('assets/forest-assets/stairs.png')
 fimg = pygame.image.load('assets/forest-assets/wall-e-2.png')
@@ -37,7 +49,23 @@ while run:
 
   win.blit(img, (400, 300))
   win.blit(bimg, (200, 300))
-  win.blit(cimg, (300, 300))
+  win.blit(cimg, (300, 230))
+  win.blit(c1img, (300, 255))
+  win.blit(c2img, (300, 270))
+  win.blit(c3img, (300, 285))
+  win.blit(c4img, (300, 300))
+  win.blit(c5img, (300, 315))
+  win.blit(c6img, (300, 330))
+  win.blit(c7img, (300, 345))
+  win.blit(c8img, (300, 360))
+  win.blit(c9img, (300, 375))
+  win.blit(c10img, (300, 390))
+
+  
+  
+  
+  
+  
   win.blit(dimg, (300, 200))
   win.blit(eimg, (300, 400))
   win.blit(fimg, (300, 250))
