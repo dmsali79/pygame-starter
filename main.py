f1775c9e46fe3651f20d439839bc05f91ef47b49
Diaco Salih <dmsali79>
2020-02-13 11:16:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ font = pygame.font.SysFont("arial", 72)
 bfont = pygame.font.SysFont("arial", 52)
 cfont = pygame.font.SysFont("arial", 42)
 
-
+x = 200
+y = 200
 
 text = font.render("The Room...", True, (255, 100, 10))
 btext = bfont.render("The Room...", True, (175, 50, 5))
 ctext = cfont.render("The Room.", True, (40, 1, 1))
+
+hero = pygame.image.load('assets/hero/sliced/idle-1.png')
+
+
 
 img = pygame.image.load('assets/forest-assets/spooky_trees.png')
 bimg = pygame.image.load('assets/forest-assets/floor.png')
@@ -30,8 +35,18 @@ while run:
       run = False
 
 # Game code starts here ---------------------
+  
+  
+  
+  
+  
   win.fill((158, 115, 55))
   
+  keys = pygame.key.get_pressed()
+
+  win.blit(hero,(x, y))
+
+
   win.blit(text, (100, 1))
   win.blit(btext, (105, 60))
   win.blit(ctext, (110, 110))
@@ -40,6 +55,9 @@ while run:
   win.blit(img, (60, 200))
   
   win.blit(bimg, (200, 350))
+  
+    
+  
 
   win.blit(cimg, (300, 280))
   win.blit(cimg, (300, 305))
@@ -52,12 +70,50 @@ while run:
   win.blit(cimg, (300, 410))
   win.blit(cimg, (300, 425))
   win.blit(cimg, (300, 440))
-  
-  
+
+
+
+
   win.blit(dimg, (300, 260))
   win.blit(eimg, (300, 450))
   win.blit(fimg, (300, 300))
 
+  
+  keys = pygame.key.get_pressed()
+
+  win.blit(hero,(x, y))
+
+
+ 
+
+  if keys[pygame.K_LEFT]:
+    hero = pygame.image.load('assets/hero/sliced/walk-4.png')
+    x -= 0.2
+    
+  
+    win.blit(hero, (x, y))
+
+
+  if keys[pygame.K_RIGHT]:
+    
+    hero = pygame.image.load('assets/hero/sliced/swim-1.png')
+    x += 0.2
+    
+   
+    win.blit(hero, (x, y))
+  
+  if keys[pygame.K_UP]:
+    hero = pygame.image.load('assets/hero/sliced/surprise.png')
+    y -=0.2
+    
+
+    win.blit(hero, (x,y))
+  
+  if keys[pygame.K_DOWN]:
+    hero = pygame.image.load('assets/hero/sliced/peace.png')
+    y +=0.2
+    win.blit(hero, (x,y))
+  
 
   # Draw a rectangle
 
