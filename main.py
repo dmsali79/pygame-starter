076b249c4e38261c13d1c6c6ad92109c33f17977
Diaco Salih <dmsali79>
2020-03-05 11:03:58 -0500

    map that covers whole screen.

--- main.py
+++ main.py
@@ -87,15 +87,53 @@ while run:
    
 
 
-   
+  for i in range(55):
+    for j in range(40):
+      win.blit (bimg, (i*15,j*15))
   
+  
+
+
+
+
   row = [1, 0, 0, 1]
 
   for i in range(4):
     if row[i] == 1:
       win.blit(bimg, (i,0))
     if row[i]==0:
-      win.blit (bimg, (i,0))
+        win.blit (text, (100,1))
+        win.blit(text, (100, 1))
+        win.blit(btext, (105, 60))
+        win.blit(ctext, (110, 110))
+
+        win.blit(img, (365, 185))
+        win.blit(img, (60, 200))
+  
+
+
+  
+
+        win.blit(cimg, (300, 280))
+        win.blit(cimg, (300, 305))
+        win.blit(cimg, (300, 320))
+        win.blit(cimg, (300, 335))
+        win.blit(cimg, (300, 350))
+        win.blit(cimg, (300, 365))
+        win.blit(cimg, (300, 380))
+        win.blit(cimg, (300, 395))
+        win.blit(cimg, (300, 410))
+        win.blit(cimg, (300, 425))
+        win.blit(cimg, (300, 440))
+
+
+
+
+        win.blit(dimg, (300, 260))
+        win.blit(eimg, (300, 450))
+        win.blit(fimg, (300, 300))
+   
+
   
 
   win.blit(hero,(x, y))
@@ -116,7 +154,7 @@ while run:
 
   if keys[pygame.K_LEFT]:
     hero = pygame.image.load('assets/hero/sliced/walk-4.png')
-    x -= 0.2
+    x -= 0.5
     
   
     win.blit(hero, (x, y))
@@ -126,21 +164,21 @@ while run:
   if keys[pygame.K_RIGHT]:
     
     hero = pygame.image.load('assets/hero/sliced/swim-1.png')
-    x += 0.2
+    x += 0.5
     
    
     win.blit(hero, (x, y))
   
   if keys[pygame.K_UP]:
     hero = pygame.image.load('assets/hero/sliced/surprise.png')
-    y -=0.2
+    y -=0.5
     
 
     win.blit(hero, (x,y))
   
   if keys[pygame.K_DOWN]:
     hero = pygame.image.load('assets/hero/sliced/peace.png')
-    y +=0.2
+    y +=0.5
     win.blit(hero, (x,y))
   
 
